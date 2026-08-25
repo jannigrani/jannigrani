c8fd93560f8cedd555ae3fcb82b00722f508d417
PB Global <318979241+pbglobalco@users.noreply.github.com>
2026-08-25 16:54:01 +0000

    refactor: update floating bottom navigation to use manual language context

--- jannigrani/src/components/layout/FloatingBottomNav.jsx
+++ jannigrani/src/components/layout/FloatingBottomNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from '../../contexts/LanguageContext';
 
 const FloatingBottomNav = () => {
   const { t } = useTranslation();
